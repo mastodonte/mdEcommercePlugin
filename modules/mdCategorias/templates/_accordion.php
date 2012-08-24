8fda3a0a20f7a08b01608800c8b3f91e19fdbd0e
Gaston Caldeiro <chugas488@gmail.com>
2012-08-24 18:09:39 -0300

    Se agrego comporamiento accordion para manejo de categorias

--- modules/mdCategorias/templates/_accordion.php
+++ modules/mdCategorias/templates/_accordion.php
@@ -1,40 +1,51 @@
 <div class="box">
-  <div class="box-heading"><?php echo $parent->getName(); ?></div>
-  <div class="box-content">
-    <div class="box-category">
-      <ul>
-      <?php foreach ($categorias as $record_categoria): ?>
-        <?php $record_categoria = $record_categoria->getRawValue(); ?>
-        <?php $hasChildren = (count($record_categoria['children']) > 0); ?>
-        
-        <li>
-          <a class="<?php echo ($record_categoria['id'] ==  $categoria->getId() ? 'active' : ''); ?>" 
-             href="<?php echo ($hasChildren && false ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $record_categoria['id'] . '&name=' . mdBasicFunction::slugify($record_categoria['name'])) . '?s=' . base64_encode($parent->getId() . '-' . $parent->getName())); ?>">
-             <?php echo $record_categoria['name']; ?>
-          </a>
+    <div class="box-heading"><?php echo $parent->getName(); ?></div>
+    <div class="box-content">
+        <div class="box-category">
+            <ul id="navigation">
+                <?php foreach ($categorias as $record_categoria): ?>
+                    <?php $record_categoria = $record_categoria->getRawValue(); ?>
+                    <?php $hasChildren = (count($record_categoria['children']) > 0); ?>
 
-          <?php if ($hasChildren): ?>
+                    <li>
+                        <a class="<?php echo ($hasChildren ? 'head' : ''); ?> <?php echo ($record_categoria['id'] == $categoria->getId() ? 'active' : ''); ?>" 
+                           href="<?php echo ($hasChildren ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $record_categoria['id'] . '&name=' . mdBasicFunction::slugify($record_categoria['name'])) . '?s=' . base64_encode($parent->getId() . '-' . $parent->getName())); ?>">
+                               <?php echo $record_categoria['name']; ?>
+                        </a>
 
-            <?php $subcategorias = $record_categoria['children']; ?>
+                        <?php if ($hasChildren): ?>
 
-            <ul>
+                            <?php $subcategorias = $record_categoria['children']; ?>
 
-              <?php foreach ($subcategorias as $subcategoria): ?>
+                            <ul>
 
-                <?php $hasChildren = (count($subcategoria['children']) > 0); ?>
+                                <?php foreach ($subcategorias as $subcategoria): ?>
 
-                <li>            
-                  <a href="<?php echo ($hasChildren && false ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $subcategoria['id'] . '&name=' . mdBasicFunction::slugify($subcategoria['name'])) . '?s=' . base64_encode($record_categoria['id'] . '-' . $record_categoria['name'])); ?>"><?php echo $subcategoria['name']; ?></a>
-                </li>
+                                    <?php $hasChildren = (count($subcategoria['children']) > 0); ?>
 
-              <?php endforeach; ?>
+                                    <li>            
+                                        <a href="<?php echo ($hasChildren && false ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $subcategoria['id'] . '&name=' . mdBasicFunction::slugify($subcategoria['name'])) . '?s=' . base64_encode($record_categoria['id'] . '-' . $record_categoria['name'])); ?>"><?php echo $subcategoria['name']; ?></a>
+                                    </li>
 
+                                <?php endforeach; ?>
+
+                            </ul>
+                        <?php endif; ?>
+                    </li>
+                <?php endforeach; ?>
             </ul>
-          <?php endif; ?>
-          
-        </li>
-      <?php endforeach; ?>
-      </ul>
+        </div>
     </div>
-  </div>
 </div>
+
+<script type="text/javascript">
+    jQuery().ready(function(){
+        jQuery('#navigation').accordion({
+            header: '.head',
+            active: false,
+            alwaysOpen: false,
+            animated: false,
+            autoheight: false
+        });
+    });
+</script>
