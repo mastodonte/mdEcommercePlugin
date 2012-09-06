3ca58b8a87286e5c00aa8f6028ec11e9b7325cd2
Gaston Caldeiro <chugas488@gmail.com>
2012-09-06 15:02:01 -0300

    css, images for category accordion management

--- modules/mdCategorias/templates/_accordion.php
+++ modules/mdCategorias/templates/_accordion.php
@@ -1,51 +1,51 @@
 <div class="box">
-    <div class="box-heading"><?php echo $parent->getName(); ?></div>
-    <div class="box-content">
-        <div class="box-category">
-            <ul id="navigation">
-                <?php foreach ($categorias as $record_categoria): ?>
-                    <?php $record_categoria = $record_categoria->getRawValue(); ?>
-                    <?php $hasChildren = (count($record_categoria['children']) > 0); ?>
+  <div class="box-heading"><?php echo $parent->getName(); ?></div>
+  <div class="box-content">
+    <div class="box-category">
+      <ul id="navigation">
+        <?php foreach ($categorias as $record_categoria): ?>
+          <?php $record_categoria = $record_categoria->getRawValue(); ?>
+          <?php $hasChildren = (count($record_categoria['children']) > 0); ?>
 
-                    <li class="<?php echo ($record_categoria['id'] == $categoria->getId() ? 'selected' : ''); ?>">
-                        <a class="<?php echo ($hasChildren ? 'head' : ''); ?> <?php echo ($record_categoria['id'] == $categoria->getId() ? 'selected' : ''); ?>" 
-                           href="<?php echo ($hasChildren ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $record_categoria['id'] . '&name=' . mdBasicFunction::slugify($record_categoria['name'])) . '?s=' . base64_encode($parent->getId() . '-' . $parent->getName())); ?>">
-                               <?php echo $record_categoria['name']; ?>
-                        </a>
+          <li class="<?php echo ($record_categoria['id'] == $categoria->getId() ? 'selected' : ''); ?>">
+            <a class="<?php echo ($hasChildren ? 'head' : ''); ?> <?php echo ($record_categoria['id'] == $categoria->getId() ? 'selected' : ''); ?>" 
+               href="<?php echo ($hasChildren ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $record_categoria['id'] . '&name=' . mdBasicFunction::slugify($record_categoria['name'])) . '?s=' . base64_encode($parent->getId() . '-' . $parent->getName())); ?>">
+                 <?php echo $record_categoria['name']; ?>
+            </a>
 
-                        <?php if ($hasChildren): ?>
+            <?php if ($hasChildren): ?>
 
-                            <?php $subcategorias = $record_categoria['children']; ?>
+              <?php $subcategorias = $record_categoria['children']; ?>
 
-                            <ul>
+              <ul>
 
-                                <?php foreach ($subcategorias as $subcategoria): ?>
+                <?php foreach ($subcategorias as $subcategoria): ?>
 
-                                    <?php $hasChildren = (count($subcategoria['children']) > 0); ?>
+                  <?php $hasChildren = (count($subcategoria['children']) > 0); ?>
 
-                                    <li>            
-                                        <a href="<?php echo ($hasChildren && false ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $subcategoria['id'] . '&name=' . mdBasicFunction::slugify($subcategoria['name'])) . '?s=' . base64_encode($record_categoria['id'] . '-' . $record_categoria['name'])); ?>"><?php echo $subcategoria['name']; ?></a>
-                                    </li>
+                  <li>            
+                    <a href="<?php echo ($hasChildren && false ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $subcategoria['id'] . '&name=' . mdBasicFunction::slugify($subcategoria['name'])) . '?s=' . base64_encode($record_categoria['id'] . '-' . $record_categoria['name'])); ?>"><?php echo $subcategoria['name']; ?></a>
+                  </li>
 
-                                <?php endforeach; ?>
-
-                            </ul>
-                        <?php endif; ?>
-                    </li>
                 <?php endforeach; ?>
-            </ul>
-        </div>
+
+              </ul>
+            <?php endif; ?>
+          </li>
+        <?php endforeach; ?>
+      </ul>
     </div>
+  </div>
 </div>
 
 <script type="text/javascript">
-    jQuery().ready(function(){
-        jQuery('#navigation').accordion({
-            header: '.head',
-            active: '#navigation li.selected a',
-            alwaysOpen: false,
-            animated: 'slide',
-            autoheight: false
-        });
+  jQuery().ready(function(){
+    jQuery('#navigation').accordion({
+      header: '.head',
+      active: '#navigation li.selected a',
+      alwaysOpen: false,
+      animated: 'slide',
+      autoheight: false
     });
+  });
 </script>
