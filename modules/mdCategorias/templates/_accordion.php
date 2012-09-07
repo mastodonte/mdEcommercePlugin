88861668398f1ae387f95c54e88b82ff34921f0f
Gaston Caldeiro <chugas488@gmail.com>
2012-09-07 14:43:07 -0300

    Accordion Management + CSS

--- modules/mdCategorias/templates/_accordion.php
+++ modules/mdCategorias/templates/_accordion.php
@@ -1,51 +1,55 @@
-<div class="box">
-  <div class="box-heading"><?php echo $parent->getName(); ?></div>
-  <div class="box-content">
-    <div class="box-category">
-      <ul id="navigation">
-        <?php foreach ($categorias as $record_categoria): ?>
-          <?php $record_categoria = $record_categoria->getRawValue(); ?>
-          <?php $hasChildren = (count($record_categoria['children']) > 0); ?>
+<div class="ecommerce-box">
 
-          <li class="<?php echo ($record_categoria['id'] == $categoria->getId() ? 'selected' : ''); ?>">
-            <a class="<?php echo ($hasChildren ? 'head' : ''); ?> <?php echo ($record_categoria['id'] == $categoria->getId() ? 'selected' : ''); ?>" 
-               href="<?php echo ($hasChildren ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $record_categoria['id'] . '&name=' . mdBasicFunction::slugify($record_categoria['name'])) . '?s=' . base64_encode($parent->getId() . '-' . $parent->getName())); ?>">
-                 <?php echo $record_categoria['name']; ?>
-            </a>
+  <div class="ecommerce-box-heading"><?php echo $parent->getName(); ?></div>
 
-            <?php if ($hasChildren): ?>
+  <ul id="ecommerce-navigation">
+    <?php foreach ($categorias as $record_categoria): ?>
+      <?php $record_categoria = $record_categoria->getRawValue(); ?>
+      <?php $hasChildren = (count($record_categoria['children']) > 0); ?>
 
-              <?php $subcategorias = $record_categoria['children']; ?>
+      <li class="<?php echo ($record_categoria['id'] == $categoria->getId() ? 'selected' : ''); ?>">
+        <a class="head <?php echo ($record_categoria['id'] == $categoria->getId() ? 'selected' : ''); ?>" 
+           href="<?php echo ($hasChildren ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $record_categoria['id'] . '&name=' . mdBasicFunction::slugify($record_categoria['name'])) . '?s=' . base64_encode($parent->getId() . '-' . $parent->getName())); ?>">
+             <?php echo $record_categoria['name']; ?>
+        </a>
 
-              <ul>
+        <?php if ($hasChildren): ?>
 
-                <?php foreach ($subcategorias as $subcategoria): ?>
+          <?php $subcategorias = $record_categoria['children']; ?>
 
-                  <?php $hasChildren = (count($subcategoria['children']) > 0); ?>
+          <ul style="display:<?php echo ($record_categoria['id'] == $categoria->getId() ? 'block' : 'none'); ?>">
 
-                  <li>            
-                    <a href="<?php echo ($hasChildren && false ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $subcategoria['id'] . '&name=' . mdBasicFunction::slugify($subcategoria['name'])) . '?s=' . base64_encode($record_categoria['id'] . '-' . $record_categoria['name'])); ?>"><?php echo $subcategoria['name']; ?></a>
-                  </li>
+            <?php foreach ($subcategorias as $subcategoria): ?>
 
-                <?php endforeach; ?>
+              <?php $hasChildren = (count($subcategoria['children']) > 0); ?>
 
-              </ul>
-            <?php endif; ?>
-          </li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
-  </div>
+              <li>            
+                <a href="<?php echo ($hasChildren && false ? 'javascript:void(0)' : url_for('@productos-categoria?id=' . $subcategoria['id'] . '&name=' . mdBasicFunction::slugify($subcategoria['name'])) . '?s=' . base64_encode($record_categoria['id'] . '-' . $record_categoria['name'])); ?>"><?php echo $subcategoria['name']; ?></a>
+              </li>
+
+            <?php endforeach; ?>
+
+          </ul>
+        <?php endif; ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+
 </div>
 
-<script type="text/javascript">
-  jQuery().ready(function(){
-    jQuery('#navigation').accordion({
-      header: '.head',
-      active: '#navigation li.selected a',
-      alwaysOpen: false,
-      animated: 'slide',
-      autoheight: false
-    });
-  });
+<script type="text/javascript">  
+$(document).ready(function(){
+  $('#ecommerce-navigation .head').click(function() {
+    // Si hago click en el mismo item seleccionado retorno
+    if($(this).hasClass('selected')) return;
+    
+    // Cierro los abiertos y le quito la clase
+    $('a.selected').removeClass('selected').next().toggle('fast');
+
+    $(this).addClass('selected').next().toggle('fast');
+
+  }).next().hide();
+  
+  $('a.selected').next().show();
+});  
 </script>
