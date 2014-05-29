5de6f83b4f8db3e4050a1e822249e58e1cd67368
Alejo Rodriguez <alejo@alejo-Lenovo-IdeaPad-Y580.(none)>
2014-05-29 11:54:17 -0300

    filters marcasbackend

--- modules/marcasBackend/templates/indexSuccess.php
+++ modules/marcasBackend/templates/indexSuccess.php
@@ -9,21 +9,8 @@
     <a href="#filterPopup" class="btn btn-info" data-toggle="modal"><i class="icon-search"></i> Filtrar</a>
   </div>
   <h1><?php echo __('Marcas_List', array(), 'messages') ?></h1>
-  <div class="pull-right">
-    <a href="#filterPopup" class="btn btn-info" data-toggle="modal"><i class="icon-search"></i> Filtrar</a>
-  </div>
 
   <?php include_partial('marcasBackend/flashes') ?>
-
-  <?php if ($configuration->hasFilterForm()): ?>
-    <?php $filterValues = $sf_user->getRawValue()->getAttribute($this->getModuleName().'.filters', array(), 'admin_module'); if (!empty($filterValues)): ?>
-    <div class="alert alert-info alert-block">
-      <a href="#" class="close fade" data-dismiss="alert">&times;</a>
-      Estos resultados estan filtrados. <a href="#filterPopup" data-toggle="modal">Modificar Filtro</a>
-    </div>
-    <?php endif; ?>
-
-  <?php endif; ?>
 
   <div id="sf_admin_header">
     <?php include_partial('marcasBackend/list_header', array('pager' => $pager)) ?>
