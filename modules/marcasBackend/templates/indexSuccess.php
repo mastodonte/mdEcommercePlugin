ef7778a0fdad6b78c239e7837d2c5e536cf70f7d
Alejo Rodriguez <alejo@alejo-Lenovo-IdeaPad-Y580.(none)>
2014-05-29 11:41:40 -0300

    brand filter fix

--- modules/marcasBackend/templates/indexSuccess.php
+++ modules/marcasBackend/templates/indexSuccess.php
@@ -6,11 +6,27 @@
 
 <div id="sf_admin_container">
   <h1><?php echo __('Marcas_List', array(), 'messages') ?></h1>
+  <div class="pull-right">
+    <a href="#filterPopup" class="btn btn-info" data-toggle="modal"><i class="icon-search"></i> Filtrar</a>
+  </div>
 
   <?php include_partial('marcasBackend/flashes') ?>
 
+  <?php if ($configuration->hasFilterForm()): ?>
+    <?php $filterValues = $sf_user->getRawValue()->getAttribute($this->getModuleName().'.filters', array(), 'admin_module'); if (!empty($filterValues)): ?>
+    <div class="alert alert-info alert-block">
+      <a href="#" class="close fade" data-dismiss="alert">&times;</a>
+      Estos resultados estan filtrados. <a href="#filterPopup" data-toggle="modal">Modificar Filtro</a>
+    </div>
+    <?php endif; ?>
+
+  <?php endif; ?>
+
   <div id="sf_admin_header">
     <?php include_partial('marcasBackend/list_header', array('pager' => $pager)) ?>
+  </div>
+  <div id="sf_admin_bar">
+    <?php include_partial('marcasBackend/filters', array('form' => $filters, 'configuration' => $configuration)) ?>
   </div>
 
   <div id="sf_admin_content">
