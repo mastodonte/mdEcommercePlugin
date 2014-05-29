37a26faec0371dc00f363f5d359a23dcd156c939
Alejo Rodriguez <alejo@alejo-Lenovo-IdeaPad-Y580.(none)>
2014-05-29 11:50:53 -0300

    brand filter fix

--- modules/marcasBackend/templates/indexSuccess.php
+++ modules/marcasBackend/templates/indexSuccess.php
@@ -5,12 +5,18 @@
 <?php slot('nav') ?><?php echo __('Home_Marcas'); ?><?php end_slot(); ?>
 
 <div id="sf_admin_container">
+  <div class="pull-right">
+    <a href="#filterPopup" class="btn btn-info" data-toggle="modal"><i class="icon-search"></i> Filtrar</a>
+  </div>
   <h1><?php echo __('Marcas_List', array(), 'messages') ?></h1>
 
   <?php include_partial('marcasBackend/flashes') ?>
 
   <div id="sf_admin_header">
     <?php include_partial('marcasBackend/list_header', array('pager' => $pager)) ?>
+  </div>
+  <div id="sf_admin_bar">
+    <?php include_partial('marcasBackend/filters', array('form' => $filters, 'configuration' => $configuration)) ?>
   </div>
 
   <div id="sf_admin_content">
